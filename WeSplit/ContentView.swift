30e7d6cf85923a9cccbbf6d376a4a321e7a478c9
Ben Atkinson <72794844+benatkinson-dev@users.noreply.github.com>
2022-04-12 09:34:31 +0100

    Update ContentView.swift

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    @State private var name = ""
+    let students = ["Harry","Hermione","Ron"]
+    @State private var selectedStudent = "Harry"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            
+            Form {
+                Section{
+                    Picker("Select your student", selection: $selectedStudent){
+                        ForEach(students, id:\.self){
+                            Text($0)
+                        }
+                    }
+                }
+                Section{
+                    TextField("Enter your name", text: $name)
+                    Text("Your name is \(name) and you selected \(selectedStudent)")
+                }
+                Section{
+                    Button("Tap count is: \(tapCount)"){
+                        tapCount += 1
+                    }
+                }
+                Section{
+                    Group{
+                        Text("Hello world!")
+                        Text ("This is my first app :)")
+                        Text("Here's to many, many,more!")
+                    }
+                    
+                }
+                
+            }.navigationTitle("My First App")
+                .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
